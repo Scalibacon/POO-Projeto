5877692017ba2b0556393cd40cac2e95e48a044b
Evandson <Evandson@DESKTOP-OB1U5GJ>
2019-05-23 15:12:21 -0300

    Relatorio 1

---
+++ src/view/Relatorio.java
@@ -0,0 +1,64 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+
+public class Relatorio extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Relatorio frame = new Relatorio();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Relatorio() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 128, 128));
+		panel.setBounds(0, 0, 795, 50);
+		contentPane.add(panel);
+		
+		JLabel lblRelatrios = new JLabel("RELAT\u00D3RIOS");
+		lblRelatrios.setForeground(Color.WHITE);
+		lblRelatrios.setFont(new Font("Tahoma", Font.PLAIN, 38));
+		panel.add(lblRelatrios);
+		
+		JButton btnNewButton = new JButton("Produtos mais vendidos");
+		btnNewButton.setBounds(298, 122, 203, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnPorPerodo = new JButton("Por per\u00EDodo");
+		btnPorPerodo.setBounds(298, 226, 203, 23);
+		contentPane.add(btnPorPerodo);
+	}
+}
