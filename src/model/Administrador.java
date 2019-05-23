b63442ca456cd3941d361571098352d81c3f138d
Evandson <Evandson@DESKTOP-OB1U5GJ>
2019-05-23 20:42:25 -0300

    metodos administrador

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Administrador extends Caixa{
+	
+	public void alterarVenda(){
+		
+	}
+	
+	public void excluirVenda(){
+		
+	}
+	
+	public void gerenciarUsuario(){
+		
+	}
+	
+	public void visualizarRelatorio(){
+		
+	}
 
 }
