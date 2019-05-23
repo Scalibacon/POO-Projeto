a11f6006e8bc631f181ded043c2f1a34fdf4ab82
Evandson <Evandson@DESKTOP-OB1U5GJ>
2019-05-23 20:52:52 -0300

    atrib getset produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Produto {
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQuantidade_estoque() {
+		return quantidade_estoque;
+	}
+	public void setQuantidade_estoque(int quantidade_estoque) {
+		this.quantidade_estoque = quantidade_estoque;
+	}
+	
+	private String nome;
+	private Categoria categoria;
+	private double preco;
+	private int quantidade_estoque;
 
 }
