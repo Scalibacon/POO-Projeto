01e27151c3b83c406eda7187d36c1c22d55461b7
Evandson <Evandson@DESKTOP-OB1U5GJ>
2019-05-23 20:54:51 -0300

    atrib getset itemvenda

--- src/model/ItemVenda.java
+++ src/model/ItemVenda.java
@@ -1,5 +1,21 @@
 package model;
 
 public class ItemVenda {
+	
+	public int getQuantidade_produto() {
+		return quantidade_produto;
+	}
+	public void setQuantidade_produto(int quantidade_produto) {
+		this.quantidade_produto = quantidade_produto;
+	}
+	public double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+	
+	private int quantidade_produto;
+	private double subtotal;
 
 }
