22a7589e530e6611bc27c9719181f09c48d0a269
Matheus de Casa <matheusferreira_mfn@hotmail.com>
2019-05-23 22:14:19 -0300

    Mudanças leves no menu e na controller dele

---
+++ src/controller/MenuController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import javax.swing.JDialog;
+
+public class MenuController {
+	public void abrirModal(JDialog tela) {
+		tela.setModal(true);
+	}
+}
--- src/view/TelaMenu.java
+++ src/view/TelaMenu.java
@@ -6,16 +6,24 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.MenuController;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.Color;
 
-public class TelaMenu extends JFrame {
+public class TelaMenu extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private MenuController controller = new MenuController();
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -39,27 +47,40 @@ public class TelaMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		ImageIcon imgEstoque = new ImageIcon(new ImageIcon("img/estoque.png").getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
+
+		ImageIcon imgEstoque = new ImageIcon(
+				new ImageIcon("img/estoque.png").getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
 		JLabel imgGerEstoque = new JLabel(imgEstoque);
 		imgGerEstoque.setBounds(57, 28, 125, 125);
 		contentPane.add(imgGerEstoque);
-		
+
 		JLabel lblGerenciarEstoque = new JLabel("Gerenciar Estoque");
 		lblGerenciarEstoque.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblGerenciarEstoque.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGerenciarEstoque.setBounds(57, 156, 125, 18);
+		lblGerenciarEstoque.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
 		contentPane.add(lblGerenciarEstoque);
-		
-		ImageIcon imgVenda = new ImageIcon(new ImageIcon("img/venda.png").getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
+
+		ImageIcon imgVenda = new ImageIcon(
+				new ImageIcon("img/venda.png").getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
 		JLabel imgRealVenda = new JLabel(imgVenda);
 		imgRealVenda.setBounds(240, 28, 125, 125);
 		contentPane.add(imgRealVenda);
-		
+
 		JLabel lblVenda = new JLabel("Realizar Venda");
 		lblVenda.setHorizontalAlignment(SwingConstants.CENTER);
 		lblVenda.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblVenda.setBounds(240, 156, 125, 18);
 		contentPane.add(lblVenda);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+
 }
