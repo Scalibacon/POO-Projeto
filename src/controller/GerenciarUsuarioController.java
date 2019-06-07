f3fc1c7bd1a981d035e8f158d44e45a561c460b9
Evandson <Evandson@DESKTOP-OB1U5GJ>
2019-06-06 21:53:46 -0300

    Alteracao grossa (table) e faltando algumas coisas pra realmente funcionar nas classes GerenciarUsuarioController e TelaGerenciarUsuario, tentei =(

--- src/controller/GerenciarUsuarioController.java
+++ src/controller/GerenciarUsuarioController.java
@@ -1,13 +1,29 @@
 package controller;
 
+import java.util.List;
+
+import javax.swing.JDialog;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
 import model.Estoquista;
+import view.TelaGerenciarUsuario2;
 
-public class GerenciarUsuarioController {
+public class GerenciarUsuarioController extends AbstractTableModel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTable tabela;
-
+	private String[] colunas = {"Nome", "CPF", "Tipo", "Telefone"};
+	private List<Estoquista> estoquista;
+	private final int COLUNA_NOME = 0;
+	private final int COLUNA_CPF = 1;
+	private final int COLUNA_TIPO = 2;
+	private final int COLUNA_TELEFONE = 3;
+	
+	
 	public GerenciarUsuarioController(JTable tabela) {
 		this.tabela = tabela;
 	}
@@ -40,5 +56,65 @@ public class GerenciarUsuarioController {
 	public void limparTabela() {
 		DefaultTableModel model = (DefaultTableModel) tabela.getModel();
 		model.setRowCount(0);
+	}
+	
+	public void adicionarOuAlterarUsuario(Estoquista e) {
+		if (e != null) {
+			JDialog tela_add_estoquista = new TelaGerenciarUsuario2();
+			tela_add_estoquista.setModal(true);
+			tela_add_estoquista.setVisible(true);
+		} else {
+			JDialog tela_add_estoquista = new TelaGerenciarUsuario2();
+			tela_add_estoquista.setModal(true);
+			tela_add_estoquista.setVisible(true);
+		}
+		fireTableDataChanged();
+	}
+	
+	public void excluirUsuario(int row) {
+		String cpf = (String) getValueAt(row, 2);
+		//Help.logado.excluirUsuario(cpf);
+		fireTableDataChanged();
+	}
+	
+	public Estoquista buscarUsuario(String cpf) {
+		for (Estoquista e : this.estoquista) {
+			if (e.getCpf().equals(cpf)) {
+				return e;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return colunas.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return estoquista.size();
+	}
+	
+	public String getColumnName(int indice) {
+		return colunas[indice];
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Estoquista estoquista = this.estoquista.get(rowIndex);
+
+		switch (columnIndex) {
+		case COLUNA_NOME:
+			return estoquista.getNome();
+		case COLUNA_CPF:
+			return estoquista.getCpf();
+		case COLUNA_TIPO:
+			return estoquista.getPrivilegio();
+		case COLUNA_TELEFONE:
+			return estoquista.getTelefone();
+		
+		}
+		return null;
 	}
 }
--- src/view/TelaGerenciarUsuario.java
+++ src/view/TelaGerenciarUsuario.java
@@ -2,21 +2,31 @@ package view;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+
 import controller.GerenciarUsuarioController;
+import model.Estoquista;
+import model.Produto;
 
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 
-public class TelaGerenciarUsuario extends JDialog {
+public class TelaGerenciarUsuario extends JDialog implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel painel;
 	private JTable tabela;
-	private GerenciarUsuarioController controller;
+	private GerenciarUsuarioController controller = new GerenciarUsuarioController(tabela);
+	private JButton btnCriar, btnExcluir1, btnAlterar1;
+
 	
 	public static void main(String args[]) {
 		TelaGerenciarUsuario2 tela = new TelaGerenciarUsuario2();
@@ -48,11 +58,87 @@ public class TelaGerenciarUsuario extends JDialog {
 		scroll.setViewportView(tabela);
 		
 		JButton btnCriar = new JButton("Criar");
-		btnCriar.setBounds(360, 90, 90, 25);
+		btnCriar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (e.getSource() == btnCriar) {
+					controller.adicionarOuAlterarUsuario(null);
+				} else if (e.getSource() == btnAlterar1) {
+					Estoquista estoquista = controller.buscarUsuario((String) controller.getValueAt(tabela.getSelectedRow(), 4));
+					controller.adicionarOuAlterarUsuario(estoquista);
+				} else if (e.getSource() == btnExcluir1) {
+					if (JOptionPane.showConfirmDialog(btnExcluir1, this, "Deseja mesmo excluir esse usuario?", getDefaultCloseOperation()) == 0) {
+						controller.excluirUsuario(tabela.getSelectedRow());
+					}
+				}
+			}
+		});
+		btnCriar.setBounds(72, 88, 90, 25);
 		painel.add(btnCriar);	
 		
 		controller = new GerenciarUsuarioController(tabela);
+		
+		JButton btnAlterar1 = new JButton("Alterar");
+		btnAlterar1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (e.getSource() == btnCriar) {
+					controller.adicionarOuAlterarUsuario(null);
+				} else if (e.getSource() == btnAlterar1) {
+					Estoquista estoquista = controller.buscarUsuario((String) controller.getValueAt(tabela.getSelectedRow(), 4));
+					controller.adicionarOuAlterarUsuario(estoquista);
+				} else if (e.getSource() == btnExcluir1) {
+					if (JOptionPane.showConfirmDialog(btnExcluir1, this, "Deseja mesmo excluir esse usuario?", getDefaultCloseOperation()) == 0) {
+						controller.excluirUsuario(tabela.getSelectedRow());
+					}
+				}
+			}
+		});
+		btnAlterar1.setBounds(358, 89, 89, 23);
+		painel.add(btnAlterar1);
+		
+		JButton btnExcluir1 = new JButton("Excluir");
+		btnExcluir1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnCriar) {
+					controller.adicionarOuAlterarUsuario(null);
+				} else if (e.getSource() == btnAlterar1) {
+					Estoquista estoquista = controller.buscarUsuario((String) controller.getValueAt(tabela.getSelectedRow(), 4));
+					controller.adicionarOuAlterarUsuario(estoquista);
+				} else if (e.getSource() == btnExcluir1) {
+					if (JOptionPane.showConfirmDialog(btnExcluir1, this, "Deseja mesmo excluir esse usuario?", getDefaultCloseOperation()) == 0) {
+						controller.excluirUsuario(tabela.getSelectedRow());
+					}
+				}
+				
+			}
+		});
+		btnExcluir1.setBounds(612, 89, 89, 23);
+		painel.add(btnExcluir1);
 		controller.iniciarTabela();
-		controller.carregarTabela();		
+		controller.carregarTabela();	
+		
 	}
+	
+	public void abrirTelaCadastro(boolean isAlterar) {
+		if (isAlterar) {
+			Estoquista estoquista = controller.buscarUsuario((String) controller.getValueAt(tabela.getSelectedRow(), 4));
+			JDialog tela_add_estoquista = new TelaGerenciarUsuario2();
+			tela_add_estoquista.setModal(true);
+			tela_add_estoquista.setVisible(true);
+		} else {
+			JDialog tela_add_produto = new TelaGerenciarUsuario2();
+			tela_add_produto.setModal(true);
+			tela_add_produto.setVisible(true);
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+		
+	
 }
